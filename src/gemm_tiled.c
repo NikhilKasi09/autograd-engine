00f7edf0cbff89516f5f3bd77a37a7b3522052ff
Nikhil Kasisomayajula <nikhil.kasisomayajula@gmail.com>
2026-06-17 19:01:50 +0100

    Completed task 5 - tiling the matrix

---
+++ src/gemm_tiled.c
@@ -0,0 +1,37 @@
+#include "gemm.h"
+#include "validate.h"
+#include <stdio.h>
+
+#define BLOCK_SIZE 64
+
+static void gemm_tiled_kernel(size_t N, const float * restrict A, const float * restrict B, float * restrict C) {
+
+    // Outer loops (slide the 64x64 window across the matrices)
+    for (size_t block_i = 0; block_i < N; block_i += BLOCK_SIZE) {
+        for (size_t block_k = 0; block_k < N; block_k += BLOCK_SIZE) {
+            for (size_t block_j = 0; block_j < N; block_j += BLOCK_SIZE) {
+
+                // Inner loops (calculate inside the current window)
+                for (size_t i = block_i; i < block_i + BLOCK_SIZE; i++) {
+                    for (size_t k = block_k; k < block_k + BLOCK_SIZE; k++) {
+                        
+                        float a_ik = A[i * N + k];
+
+                        for (size_t j = block_j; j < block_j + BLOCK_SIZE; j++) {
+                            C[i * N + j] += a_ik * B[k * N + j];
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+
+void gemm_tiled(const matrix_t *A, const matrix_t *B, matrix_t *C) {
+    if (A->size != B->size || A->size != C->size) {
+        return; 
+    }
+    gemm_tiled_kernel(A->size, A->data, B->data, C->data);
+}
